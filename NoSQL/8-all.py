369f0738d65eec79fe6d9282aa0d4416a72d2ee0
metanetabasova <metanetabasova314@gmail.com>
2026-07-17 16:13:51 -0300

    Add

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python3
+"""
+Modul sənədləşdirməsi (Module documentation)
+Kolleksiyadakı sənədləri siyahılamaq üçün funksiya.
+"""
+
+
 def list_all(mongo_collection):
     """
-    Kolleksiyadakı bütün sənədləri siyahı şəklində qaytarır.
-    Əgər sənəd yoxdursa, boş siyahı ([]) qaytarır.
+    List all documents in a collection.
+    Return an empty list if no document in the collection.
     """
     if mongo_collection is None:
         return []
-    
     return list(mongo_collection.find())
