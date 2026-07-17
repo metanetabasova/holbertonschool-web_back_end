93691c29eb5f626fb912dcccb8bdbbfb208f8da4
metanetabasova <metanetabasova314@gmail.com>
2026-07-17 15:54:16 +0000

    Add

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,76 +1,28 @@
-#!/usr/bin/env python3
-"""
-Deletion-resilient hypermedia pagination
-"""
-import csv
-import math
-from typing import List, Dict, Any
+from typing import Dict, List
 
 
-class Server:
-    """Server class to paginate a database of popular baby names.
+def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
     """
-    DATA_FILE = "Popular_Baby_Names.csv"
+    Return a page of data that is resilient to deletions.
+    """
 
-    def __init__(self):
-        self.__dataset = None
-        self.__indexed_dataset = None
+    dataset = self.indexed_dataset()
 
-    def dataset(self) -> List[List]:
-        """Cached dataset
-        """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+    assert index is not None
+    assert 0 <= index < max(dataset.keys()) + 1
 
-        return self.__dataset
+    data = []
+    current_index = index
 
-    def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
-        if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
-        return self.__indexed_dataset
+    while len(data) < page_size and current_index <= max(dataset.keys()):
+        if current_index in dataset:
+            data.append(dataset[current_index])
 
-    def get_hyper_index(
-        self,
-        index: int = None,
-        page_size: int = 10
-    ) -> Dict[str, Any]:
-        """
-        Silinmələrə qarşı dayanıqlı indeks əsaslı səhifələmə.
-        İstifadəçinin elementləri qaçırmamasını təmin edir.
-        """
-        indexed_data = self.indexed_dataset()
+        current_index += 1
 
-        # assert yoxlamalarını sətirlərə bölərək tam qısaldırıq
-        if index is not None:
-            assert isinstance(index, int)
-            assert 0 <= index < len(indexed_data)
-
-        assert isinstance(page_size, int) and page_size > 0
-
-        if index is None:
-            index = 0
-
-        data_page = []
-        current_index = index
-
-        while len(data_page) < page_size and current_index < len(indexed_data):
-            if current_index in indexed_data:
-                data_page.append(indexed_data[current_index])
-            current_index += 1
-
-        next_index = current_index if current_index < len(indexed_data) else None
-
-        return {
-            "index": index,
-            "next_index": next_index,
-            "page_size": len(data_page),
-            "data": data_page
-        }
+    return {
+        "index": index,
+        "data": data,
+        "page_size": len(data),
+        "next_index": current_index
+    }
