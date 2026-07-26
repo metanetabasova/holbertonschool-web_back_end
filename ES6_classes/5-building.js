51b85fbafe0105fcb0389a02c35648d7289d0663
metanetabasova <metanetabasova314@gmail.com>
2026-07-26 18:41:08 +0000

    Add

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+  
+    // Sqft getter
+  get sqft() {
+    return this._sqft;
+  }
+}
