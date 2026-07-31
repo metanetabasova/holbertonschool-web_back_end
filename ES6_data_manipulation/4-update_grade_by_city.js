868da98416d7669d75b451db72df304bd4eda7ca
metanetabasova <metanetabasova314@gmail.com>
2026-07-31 13:47:00 +0000

    Add

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,18 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
+export default function updateStudentGradeByCity(students, city, newGrades = []) {
   if (!Array.isArray(students)) {
     return [];
   }
-
+  
+  const grades = Array.isArray(newGrades) ? newGrades : [];
+  
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const gradeObj = Array.isArray(newGrades)
-        ? newGrades.find((grade) => grade.studentId === student.id)
-        : null;
-
+      const studentGrade = grades.filter((grade) => grade.studentId === student.id);
+        
       return {
         ...student,
-        garde: gardeObj ? gradeObj.grade : 'N/A',
+        grade: studentGrade.length > 0 ? studentGrade[0].grade : 'N/A',
       };
     });
 }
